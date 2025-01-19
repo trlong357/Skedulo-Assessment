ca28f03c08a2bc13a3a24ddd96d98b1f515ebdba
Tony <longtran@Longs-MacBook-Pro.local>
2025-01-19 19:09:05 +0700

    add description

---
+++ README.md
@@ -0,0 +1,21 @@
+# Skedulo Assessments
+
+This is assetsments of skedulo for FE Engineer position
+
+## Demo
+
+You can view the live demo of the project at the following link:
+
+[Live Demo](https://github-cowmj32j5-tony-trans-projects-c4b6eb87.vercel.app/)
+
+## Screenshot
+
+Here’s a screenshot of the application:
+
+![Screenshot](test_result.png)
+
+## Question 2
+
+- Time complexity: O(n log n)
+
+- Space complexity: O(n)
--- test2.ts
+++ test2.ts
@@ -12,12 +12,28 @@ let test2 = [
   { startPx: 20, endPx: 40 },
 ];
 
+let test3 = [
+  { startPx: 10, endPx: 30 },
+  { startPx: 55, endPx: 65 },
+  { startPx: 35, endPx: 50 },
+  { startPx: 20, endPx: 10 },
+  { startPx: 60, endPx: 70 },
+  { startPx: 90, endPx: 40 },
+];
+
 const unionItems = (
   items: {
     startPx: number;
     endPx: number;
   }[]
 ) => {
+  // Ensure each interval is valid by swapping start and end if end < start
+  items.forEach((interval) => {
+    if (interval.endPx < interval.startPx) {
+      [interval.startPx, interval.endPx] = [interval.endPx, interval.startPx];
+    }
+  });
+
   items.sort((a, b) => a.startPx - b.startPx);
   let result: {
     startPx: number;
@@ -41,3 +57,4 @@ const unionItems = (
 
 console.log(unionItems(test1));
 console.log(unionItems(test2));
+console.log(unionItems(test3));
